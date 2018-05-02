d548791b842c1c74cd1ced42101d85349c9691b7
Daan Kort <daankort3@gmail.com>
2018-05-02 15:41:17 +0200

    Made it even fancier

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function reqDeck () {
 	let json = JSON.parse( this.responseText );
 	let deck_id = json.deck_id;
 	console.log(deck_id);
-  }
+}
 
 let define = {
 	deckId:undefined,
@@ -19,8 +19,7 @@ function getDeck(url){
 		define.deckId = myJson.deck_id;
 		getCards(`https://deckofcardsapi.com/api/deck/${define.deckId}/draw/?count=9`);
 	});
-
-  }
+}
 
 function getCards(url){
 	fetch(url)
@@ -31,21 +30,31 @@ function getCards(url){
 		define.deck = myJson;
 		console.log(define);
 	});
-  }
+}
 
-  function showCard () {
-	let x = document.createElement("IMG");
-	let arr = define.deck.cards;
-	arr = arr[Math.floor((Math.random() * arr.length) )].image;
-	rndCard = '<img class="card"  src="' + arr + '" />';
-	document.querySelector("#app").innerHTML = rndCard;
-  }
-  
-  const show = document.querySelector( "#random" );
-  show.addEventListener('click' ,function (event){
+function showCard () {
+	(async () => {
+		document.querySelector( "#random" ).disabled =true;
+		let app = document.querySelector("#app");
+		let element = document.createElement("div");
+		element.classList.add("loader");
+		app.innerHTML = '';
+		app.appendChild(element);
+		await new Promise(resolve => setTimeout(resolve, (Math.random()*2000)+1));
+		let arr = define.deck.cards;
+		arr = arr[Math.floor((Math.random() * arr.length) )].image;
+		rndCard = '<img class="card"  src="' + arr + '" />';
+		app.innerHTML = rndCard;
+
+		document.querySelector( "#random" ).disabled = false;
+	})();
+	
+}
+
+const show = document.querySelector( "#random" );
+show.addEventListener('click' ,function (event){
 	showCard();
-  });
-  
-  getDeck('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-  //getResponse('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-  
+});
+
+getDeck('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+
